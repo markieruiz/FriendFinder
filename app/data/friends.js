c7a97acc81eccd750dff6ebcf4053c656bc1ecad
Markie Ruiz <markieruiz@Markies-MacBook-Pro.local>
2019-01-18 20:24:35 -0600

    adding friends

---
+++ app/data/friends.js
@@ -0,0 +1,72 @@
+var friends = [
+  {
+    name: "Jason Momoa",
+    photo: "https://pixel.nymag.com/imgs/daily/vulture/2018/12/06/6-momoa.w330.h330.jpg",
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+
+  {
+    name: "Julia Roberts",
+    photo: "http://www.eduinreview.com/blog/wp-content/uploads/2010/08/julia-roberts.jpg",
+    scores: [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
+  },
+
+  {
+    name: "Lady Gaga",
+    photo: "https://cdn1.thr.com/sites/default/files/imagecache/portrait_300x450/2016/02/gettyimages-513037310-p_2016.jpg",
+    scores: [
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4"
+    ]
+  } ,
+
+  {
+    name: "Sponge Bob",
+    photo: "https://cdn.thinglink.me/api/image/880442828625281024/320/10/scaletowidth?d=404",
+    scores: [
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+  }
+
+];
+
+module.exports = friends;
